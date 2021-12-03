df59f5642b02d2c34698290b29b0919a3d939637
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-12-02 18:16:20 -0600

    searchbar supp

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Avatar from '../components/Avatar'
+import { ViewGridIcon, MicrophoneIcon, SearchIcon } from '@heroicons/react/solid';
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -16,10 +18,24 @@ export default function Home() {
           <div className="flex items-center space-x-4">
             <p className="cursor-pointer hover:underline">Gmail</p>
             <p className="cursor-pointer hover:underline">Images</p>
+
+            <ViewGridIcon className="w-10 h-10 p-2 rounded-full cursor-pointer hover:bg-gray-200" />
+            
             <Avatar url="https://avatars.githubusercontent.com/u/75763058?s=96&v=4" />
 
           </div>
       </header>
+      <form>
+        <Image src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+        height={100}
+        width={300} />
+
+        <div className="flex items-center w-full max-w-md px-5 py-3 mt-5 border border-gray-300 rounded-full hover:shadow-lg focus-within:shadow-lg">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow p-2 rounded-full focus:outline-none" />
+          <MicrophoneIcon className="h-5 mr-3 text-gray-500" />
+        </div>
+      </form>
 
 
       
