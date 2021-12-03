a9eb5dd175027a8dfec33383e4ba8e4a491ad194
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-12-02 22:57:38 -0600

    naming

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
-        <title>Google 3.0</title>
+        <title>Hatsune Miku X Google </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className="flex justify-between w-full p-5 text-gray-700">
