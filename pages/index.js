84332f21bace6dc501760ee7c9bff120718fc493
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-12-02 18:30:35 -0600

    ✨ main page

--- pages/index.js
+++ pages/index.js
@@ -25,15 +25,24 @@ export default function Home() {
 
           </div>
       </header>
-      <form>
+      <form className="flex flex-col items-center flex-grow mt-44">
         <Image src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
         height={100}
         width={300} />
 
-        <div className="flex items-center w-full max-w-md px-5 py-3 mt-5 border border-gray-300 rounded-full hover:shadow-lg focus-within:shadow-lg">
-          <SearchIcon className="h-5 mr-3 text-gray-500" />
-          <input type="text" className="flex-grow p-2 rounded-full focus:outline-none" />
-          <MicrophoneIcon className="h-5 mr-3 text-gray-500" />
+        <div className="flex items-center w-full max-w-md px-5 py-3 mt-5 border border-gray-300 rounded-full hover:shadow-lg focus-within:shadow-lg sm:max-w-xl lg:max-w-2xl">
+            <SearchIcon className="h-5 mr-3 text-gray-500" />
+            <input type="text" className="flex-grow p-2 rounded-full focus:outline-none" />
+            <MicrophoneIcon className="h-5 mr-3 text-gray-500" />
+        </div>
+        <div className="flex flex-col justify-center w-1/2 mt-8 space-y-2 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="bg-[#f8f9fa] p-3 rounded-md ring-gray-200 text-sm text-gray-800 cursor-pointer hover:ring-1 hover:shadow-md focus:outline-none active:ring-gray-300">
+            Google Search
+          </button>
+          <button className="hover:shadow-md bg-[#f8f9fa] p-3 rounded-md ring-gray-200 text-sm text-gray-800 cursor-pointer hover:ring-1 focus:outline-none active:ring-gray-300">
+            Feeling Lucky
+          </button>
+
         </div>
       </form>
 
